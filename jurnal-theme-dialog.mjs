b529b45090ff8752de5155693a18cb08102d98d4
BartlomiejBielecki <bartlomiejbielecki@gmail.com>
2025-10-24 10:09:01 +0200

    creatin module

---
+++ jurnal-theme-dialog.mjs
@@ -0,0 +1,95 @@
+export class JournalThemeDialog extends foundry.applications.api.ApplicationV2 {
+  static DEFAULT_OPTIONS = {
+    actions: {
+      applyTheme: JournalThemeDialog.#applyTheme,
+    },
+    position: {
+      width: 550,
+      height: "auto",
+    },
+    template:
+      "systems/mutants-and-masterminds-3e/templates/dialog/jurma-theme.hbs",
+    window: { title: "JT.JURNAL.title" },
+  };
+
+  async _renderHTML() {
+    console.log(this);
+    const path = this.options.template;
+
+    const fonts = {
+      arial: "Arial",
+      poppins: "Poppins",
+      robotoMono: "Roboto Mono",
+      tektur: "Tektur",
+      josefinSans: "Josefin Sans",
+      goldman: "Goldman",
+      prompt: "Prompt",
+      russoOne: "Russo One",
+      righteous: "Righteous",
+      quantico: "Quantico",
+      secularOne: "Secular One",
+    };
+    const userID = game.user.id;
+    const journalUuid = this.options.uuid;
+    const journalEntry = await fromUuid(journalUuid);
+    const headerFont = CONFIG.JT.JurnalHeaderFont;
+    const flagTheme = journalEntry?.flags?.JT?.[userID]?.theme;
+    const flagHederFont = journalEntry?.flags?.JT?.[userID]?.headerFont;
+    const flagBodyFont = journalEntry?.flags?.JT?.[userID]?.bodyFont;
+    const data = {
+      listtheme: CONFIG.JT.sheetTheme, // or whatever list you have
+      headerFont: headerFont, // header fonts
+      textFont: fonts,
+      selectedTheme: flagTheme,
+      selectedHederFont: flagHederFont,
+      selectedBodyFont: flagBodyFont, // body fonts
+    };
+
+    let html;
+    if (game.release.generation > 12) {
+      html = foundry.applications.handlebars.renderTemplate(path, data);
+    } else {
+      html = renderTemplate(path, data);
+    }
+
+    return html;
+  }
+
+  async _replaceHTML(result, html) {
+    html.innerHTML = result;
+  }
+
+  static async #applyTheme() {
+    const element = this.element;
+    const journalUuid = this.options.uuid;
+    const selectors = element.querySelectorAll("select");
+    let userID = game.user.id;
+    const newValues = {};
+    selectors.forEach((select) => {
+      const id = select.id; 
+      const value = select.value;
+      newValues[id] = value;
+    });
+    const isGM = game.user.isGM;
+    if (journalUuid !== undefined) {
+      const journalEntry = await fromUuid(journalUuid);
+      const updateData = {};
+      if(isGM){
+        userID = "default"
+      }
+      Object.entries(newValues).forEach(([key, value]) => {
+        updateData[`flags.JT.${userID}.${key}`] = value;
+      });
+      if (Object.keys(updateData).length > 0) {
+        await journalEntry.update(updateData);
+      }
+    } else {
+      if (Object.keys(newValues).length > 0) {
+        const currentDefaults =
+          game.settings.get("journal-theme", "GMdefoultTheme") || {};
+        const merged = { ...currentDefaults, ...newValues };
+        await game.settings.set("journal-theme", "GMdefoultTheme", merged);
+      }
+    }
+  }
+}
---
+++ main.mjs
@@ -0,0 +1,119 @@
+import { JT } from "./config.mjs";
+import { JournalThemeDialog } from "./jurnal-theme-dialog.mjs";
+
+Hooks.once("init", async function () {
+  CONFIG.JT = JT;
+  game.settings.register("journal-theme", "minOwnershipLevel", {
+    name: "JT.SETTING.MinOwnership", // left-hand label
+    hint: "JT.SETTING.MinOwnershipHint", // subtext hint
+    scope: "world", // global for all users
+    config: true, // show in Module Settings
+    type: Number, // stored as number
+    choices: {
+      1: "Limited", // user-friendly label
+      2: "Observer",
+      3: "Owner",
+    },
+    default: 3, // default = Owner
+  });
+  // Register the stored default theme for GM
+  game.settings.register("journal-theme", "GMdefoultTheme", {
+    name: "JT.SETTING.GMdefoultTheme",
+    hint: "JT.SETTING.GMdefoultThemeHint",
+    scope: "world",
+    config: false,
+    default: {},
+    type: Object,
+  });
+
+  // Register menu entry (button) in the Settings UI
+  game.settings.registerMenu("journal-theme", "themeMenu", {
+    name: "JT.MENU.ThemeConfig", // shown as the left label
+    label: "JT.MENU.OpenDialog", // the button text
+    hint: "JT.MENU.ThemeConfigHint", // subtext hint
+    icon: "fas fa-palette", // small icon on the button
+    type: JournalThemeDialog, // this is your dialog class
+    restricted: true, // true = GM only, false = everyone
+  });
+});
+
+Hooks.on("renderJournalEntrySheet", (html) => {
+  let element;
+  let uuid;
+  if (game.release.generation < 13) {
+    element = html[0];
+    uuid = html.document.uuid;
+  } else {
+    element = html.element;
+    uuid = html.document.uuid;
+  }
+  const userID = game.user.id;
+  const ownerShipSettings = game.settings.get(
+    "journal-theme",
+    "minOwnershipLevel",
+  );
+  const journalOwnerShip = html.document.ownership[userID];
+  if (ownerShipSettings === journalOwnerShip) {
+    const header = element.querySelector(".window-header");
+    const bbuttenexist = element.querySelector(
+      ".header-control.icon.fa-solid.fa-palette",
+    );
+    if (bbuttenexist === null) {
+      const title = header.querySelector("h1");
+      const btn = document.createElement("button");
+      btn.type = "button";
+      btn.classList.add("header-control", "icon", "fa-solid", "fa-palette");
+      btn.dataset.tooltip = game.i18n.localize("JT.JURNAL.ThemeTooltip");
+      title.insertAdjacentElement("afterend", btn);
+      btn.addEventListener("click", (ev) => {
+        ev.stopPropagation();
+        const data = { element: element, uuid: uuid };
+        const dialog = new JournalThemeDialog(data);
+        dialog.render(true);
+      });
+    }
+  }
+  const gmDefault = game.settings.get("journal-theme", "GMdefoultTheme") || {};
+  const jtFlags = html.document?.flags?.JT || {};
+  const flags = jtFlags[userID] ?? jtFlags["default"] ?? {};
+  const flagTheme = flags.theme ?? gmDefault.theme;
+  const flagHeaderFont = flags.headerFont ?? gmDefault.headerFont;
+  const flagBodyFont = flags.bodyFont ?? gmDefault.bodyFont;
+  const fonts = {
+    arial: "Arial",
+    poppins: "Poppins",
+    robotoMono: "Roboto Mono",
+    tektur: "Tektur",
+    josefinSans: "Josefin Sans",
+    goldman: "Goldman",
+    prompt: "Prompt",
+    russoOne: "Russo One",
+    righteous: "Righteous",
+    quantico: "Quantico",
+    secularOne: "Secular One",
+  };
+  if (flagBodyFont !== undefined) {
+    const pages = element.querySelectorAll(".journal-entry-pages");
+    pages.forEach((page) => {
+      page.style.fontFamily = fonts[flagBodyFont];
+    });
+  }
+  if (flagHeaderFont !== undefined) {
+    const elements = element.querySelectorAll(
+      ".window-title, .title, .sidebar.journal-sidebar.flexcol, .page-heading, .journal-page-header h1",
+    );
+    elements.forEach((element) => {
+      if (flagHeaderFont !== "") {
+        // set the font with !important
+        element.style.setProperty("font-family", flagHeaderFont, "important");
+      } else {
+        // remove any previously set inline font
+        element.style.removeProperty("font-family");
+      }
+    });
+  }
+  if (flagTheme !== undefined) {
+    //apply Theme
+    element.className = `application sheet journal-sheet journal-entry expanded ${flagTheme}`;
+  }
+});
